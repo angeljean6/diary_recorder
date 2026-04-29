0e9012e4c290610bb0db7e74d658b8ed2c29aa28
angeljean6 <ajrescober@gmail.com>
2026-04-29 23:14:53 +0800

    added header and method call

--- main_p3.py
+++ main_p3.py
@@ -2,3 +2,6 @@ from journal_recorder import JournalRecorder
 
 def main():
     my_journal = JournalRecorder("mylife.txt")
+
+    print("--- Start Journal Entry ---")
+    my_journal.record_multiple_lines()
